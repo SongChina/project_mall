651138155e804894a9e44a3659eec125de319cde
ZYP6 <473774494@qq.com>
2019-07-04 19:05:58 +0800

    新增推广

---
+++ src/main/java/com/cskaoyan/mallSpringboot/controller/promotion/GrouponController.java
@@ -0,0 +1,10 @@
+package com.cskaoyan.mallSpringboot.controller.promotion;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("groupon")
+public class GrouponController {
+
+}
