b60fbda25b6fc8542f4a778342c1043cc6c05c1a
Matrix2049 <554858528@qq.com>
2019-07-05 16:01:41 +0800

    配置管理完成

--- src/main/java/com/cskaoyan/mallSpringboot/controller/config/ConfigController.java
+++ src/main/java/com/cskaoyan/mallSpringboot/controller/config/ConfigController.java
@@ -1,6 +1,6 @@
 package com.cskaoyan.mallSpringboot.controller.config;
 
-import com.cskaoyan.mallSpringboot.service.ConfigService;
+import com.cskaoyan.mallSpringboot.service.config.ConfigService;
 import com.cskaoyan.mallSpringboot.util.JacksonUtil;
 import com.cskaoyan.mallSpringboot.vo.ResponseVo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +22,42 @@ public class ConfigController {
     }
     @PostMapping("/mall")
     public ResponseVo updataMall(@RequestBody String body){
+        Map<String,String> data= JacksonUtil.toMap(body);
+        ResponseVo responseVo=configService.updateConfig(data);
+        return responseVo;
+    }
+
+    @GetMapping("/express")
+    public ResponseVo listExpress(){
+        ResponseVo responseVo=configService.listExpress();
+        return responseVo;
+    }
+    @PostMapping("/express")
+    public ResponseVo updataExpress(@RequestBody String body){
+        Map<String,String> data= JacksonUtil.toMap(body);
+        ResponseVo responseVo=configService.updateConfig(data);
+        return responseVo;
+    }
+
+    @GetMapping("/order")
+    public ResponseVo listOrder(){
+        ResponseVo responseVo=configService.listOrder();
+        return responseVo;
+    }
+    @PostMapping("/order")
+    public ResponseVo updataOrder(@RequestBody String body){
+        Map<String,String> data= JacksonUtil.toMap(body);
+        ResponseVo responseVo=configService.updateConfig(data);
+        return responseVo;
+    }
+
+    @GetMapping("/wx")
+    public ResponseVo listWx(){
+        ResponseVo responseVo=configService.listWx();
+        return responseVo;
+    }
+    @PostMapping("/wx")
+    public ResponseVo updataWx(@RequestBody String body){
         Map<String,String> data= JacksonUtil.toMap(body);
         ResponseVo responseVo=configService.updateConfig(data);
         return responseVo;
