a6333ea9b01342cd04b8e5bee5edac39c6a54655
renroot <815443129@qq.com>
2019-07-09 09:52:51 +0800

    日志基本完成

--- src/main/java/com/cskaoyan/mallSpringboot/controller/LogController.java
+++ src/main/java/com/cskaoyan/mallSpringboot/controller/LogController.java
@@ -1,6 +1,9 @@
 package com.cskaoyan.mallSpringboot.controller;
 
-import com.cskaoyan.mallSpringboot.vo.ResponseVo;
+
+import com.cskaoyan.mallSpringboot.renguopingVO.ResponseVo;
+import com.cskaoyan.mallSpringboot.service.LogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +11,10 @@ import java.util.HashMap;
 
 @RestController
 public class LogController {
+    @Autowired
+    LogService logService;
+
+    ResponseVo responseVo;
 
     @RequestMapping("test")
     public String test(){
@@ -16,7 +23,7 @@ public class LogController {
 
     @RequestMapping("auth/login")
     public ResponseVo login(){
-        ResponseVo responseVo = new ResponseVo();
+        responseVo = new ResponseVo();
         responseVo.setErrno(0);
         responseVo.setData("02514c8b-4124-4a95-b513-e77ef43403ba");
         responseVo.setErrmsg("成功");
@@ -24,7 +31,7 @@ public class LogController {
     }
     @RequestMapping("auth/info")
     public ResponseVo dashboard(){
-        ResponseVo responseVo = new ResponseVo();
+        responseVo = new ResponseVo();
         responseVo.setErrno(0);
         HashMap<String, Object> para = new HashMap<>();
         para.put("perms", "*");
@@ -33,6 +40,13 @@ public class LogController {
         para.put("avatar", "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
         responseVo.setData(para);
         responseVo.setErrmsg("成功");
+        return responseVo;
+    }
+
+    @RequestMapping("log/list")
+    public ResponseVo queryList(int page,int limit,String admin){
+        responseVo=new ResponseVo();
+        responseVo=logService.queryList(page,limit,admin);
         return responseVo;
     }
 }
---
+++ src/main/java/com/cskaoyan/mallSpringboot/service/LogServiceImpl.java
@@ -0,0 +1,44 @@
+package com.cskaoyan.mallSpringboot.service;
+
+import com.cskaoyan.mallSpringboot.bean.Log;
+import com.cskaoyan.mallSpringboot.mapper.LogMapper;
+import com.cskaoyan.mallSpringboot.renguopingVO.ResponseVo;
+import com.cskaoyan.mallSpringboot.renguopingVO.ResultVo;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LogServiceImpl implements LogService {
+    @Autowired
+    LogMapper logMapper;
+
+    ResponseVo responseVo;
+    ResultVo resultVo;
+
+    @Override
+    public ResponseVo queryList(int page, int limit, String admin) {
+        responseVo=new ResponseVo();
+        resultVo=new ResultVo();
+        if(admin==null){
+            PageHelper.startPage(page,limit);
+            List<Log> logs=logMapper.queryAllLog();
+            PageInfo<Log> pageInfo=new PageInfo<>(logs);
+            resultVo.setTotal((int)pageInfo.getTotal());
+            resultVo.setItems(pageInfo.getList());
+        }else{
+            PageHelper.startPage(page,limit);
+            List<Log> logs=logMapper.queryByName(admin);
+            PageInfo<Log> pageInfo=new PageInfo<>(logs);
+            resultVo.setTotal((int)pageInfo.getTotal());
+            resultVo.setItems(pageInfo.getList());
+        }
+        responseVo.setErrno(0);
+        responseVo.setData(resultVo);
+        responseVo.setErrmsg("成功");
+        return responseVo;
+    }
+}
